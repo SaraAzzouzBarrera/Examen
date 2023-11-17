dc83d765f85960e08a5fbea7165759f4bc6f674e
SaraAzzouzBarrera <sara.azzouz@estduanits.urv.cat>
2023-11-17 10:39:49 +0100

    commit6

--- harryPotterApi.js
+++ harryPotterApi.js
@@ -1,5 +1,5 @@
 const URL_API= 'https://wizard-world-api.herokuapp.com';
-
+const idTable= [];
 window.onload= async() => {
     const characters= await getCharacters();
 
@@ -8,23 +8,21 @@ window.onload= async() => {
         const newElement= document.createElement("div");
         newElement.innerHTML=`
             <h3>Character: ${character.lastName}</h3>`;
-    
-        for (const elixir of character.elixirs) {
-        const htmlElement2= document.getElementById("elixirs");
-        const newElement2= document.getElementById("div")
-        newElement2.innerHTML=`<p>Elixirs: ${elixir.name}`
-        htmlElement.appendChild(newElement);
-        htmlElement2.appendChild(newElement2);
-        
-        }
-           
-    }
-    
-        
-    
-    
+            htmlElement.appendChild(newElement);
+            
+            for (const elixir of character.elixirs) {
+        const newElement2= document.createElement("div")
+        newElement2.innerHTML=`<h5>Elixirs: ${elixir.name}</h5>`
+        idTable.push(`${elixir.id}`);
+        console.log(idTable);
+        htmlElement.appendChild(newElement2);   
+            }      
+    }     
 };
 
+ingredientsButton.addEventListener('click', () => {
+	
+});
 
 async function getCharacters(){
     const response= await fetch(`${URL_API}/wizards`);
@@ -32,8 +30,8 @@ async function getCharacters(){
     return data;
 }
 
-async function getElixirs(){
-    const response= await fetch(`${URL_API}/wizards`);
-    const data= await response.json()
-    return data.elixirs;
+function getElixirsId(id){
+    if(idTable.find(id)){
+        alert("Id trobat");     
+    }
 }

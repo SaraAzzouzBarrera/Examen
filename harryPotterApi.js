564278aecc3476ece60f0593476116f61e0c0761
SaraAzzouzBarrera <sara.azzouz@estduanits.urv.cat>
2023-11-17 09:20:12 +0100

    Commit3

--- harryPotterApi.js
+++ harryPotterApi.js
@@ -8,17 +8,15 @@ window.onload= async() => {
         const htmlElement= document.getElementById("characters");
         const newElement= document.createElement("div");
         newElement.innerHTML=`
-            <h3>Peronatge ${i}: ${character.firstName} ${character.lastName}</h3>
-        `
-        i++;
-
+            <h3>Character: ${character.lastName}</h3>`
         htmlElement.appendChild(newElement);
     }
 
 };
 
 async function getCharacters(){
-    const response= await fetch(`${URL_API}/houses`);
-    const data= response.json()
-    return data.heads;
+    const response= await fetch(`${URL_API}/wizards`);
+    const data= await response.json()
+    return data;
 }
+

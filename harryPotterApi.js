67aa9c52e021c44610923533e0cf79bc62271b45
SaraAzzouzBarrera <sara.azzouz@estduanits.urv.cat>
2023-11-17 10:02:05 +0100

    Commit5

--- harryPotterApi.js
+++ harryPotterApi.js
@@ -2,20 +2,25 @@ const URL_API= 'https://wizard-world-api.herokuapp.com';
 
 window.onload= async() => {
     const characters= await getCharacters();
-    const elixirs= await getElixirs();
 
     for(const character of characters){
         const htmlElement= document.getElementById("characters");
         const newElement= document.createElement("div");
         newElement.innerHTML=`
-            <h3>Character: ${character.lastName}</h3>`
-            htmlElement.appendChild(newElement);
-    }
+            <h3>Character: ${character.lastName}</h3>`;
     
+        for (const elixir of character.elixirs) {
         const htmlElement2= document.getElementById("elixirs");
         const newElement2= document.getElementById("div")
-            newElement2.innerHTML=`<p>Elixirs: ${elixirs.name}`
-            htmlElement2.appendChild(newElement2);
+        newElement2.innerHTML=`<p>Elixirs: ${elixir.name}`
+        htmlElement.appendChild(newElement);
+        htmlElement2.appendChild(newElement2);
+        
+        }
+           
+    }
+    
+        
     
     
 };

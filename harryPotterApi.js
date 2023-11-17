aeb7a5d2913db78e08bcd4d9c79d1f8e12609209
SaraAzzouzBarrera <sara.azzouz@estduanits.urv.cat>
2023-11-17 11:34:28 +0100

    commit8

--- harryPotterApi.js
+++ harryPotterApi.js
@@ -1,8 +1,8 @@
 const URL_API= 'https://wizard-world-api.herokuapp.com';
-const idTable= [];
+
 window.onload= async() => {
     const characters= await getCharacters();
-
+    const cases= await casesHogwarts();
     for(const character of characters){
         const htmlElement= document.getElementById("characters");
         const newElement= document.createElement("div");
@@ -12,26 +12,47 @@ window.onload= async() => {
             
             for (const elixir of character.elixirs) {
         const newElement2= document.createElement("div")
-        newElement2.innerHTML=`<h5>Elixirs: ${elixir.name}</h5>`
-        idTable.push(`${elixir.id}`);
-        console.log(idTable);
+        newElement2.innerHTML=`<h5>Elixirs: ${elixir.name}</h5>
+        <button onclick= "elixir('${elixir.id}')">Ingredients</button>
+        `
         htmlElement.appendChild(newElement2);   
-            }      
-    }     
+         }    
+    }
+    for(const house of cases){
+    const htmlElement6= document.getElementById("house");
+    const newElement6= document.createElement("div");
+    newElement6.innerHTML=`
+        <h3>House: ${house.name}</h3>`
+    
+        htmlElement6.appendChild(newElement6);
+    }
+    
 };
-
-ingredientsButton.addEventListener('click', () => {
-	
-});
 
 async function getCharacters(){
     const response= await fetch(`${URL_API}/wizards`);
     const data= await response.json()
     return data;
 }
+async function getIngredients(id){
+    const response= await fetch(`${URL_API}/elixirs/${id}`);
+    const data= await response.json()
+    return data;
+}
 
-function getElixirsId(id){
-    if(idTable.find(id)){
-        alert("Id trobat");     
-    }
+async function elixir(id){
+    const ingredients= await getIngredients(id);
+        for(const ingred of ingredients.ingredients){
+            const htmlElement= document.getElementById("characters");
+
+            const newElement3= document.createElement("div");
+            newElement3.innerHTML=`<h3>idIngredient: ${ingred.name}</h3>`
+            htmlElement.appendChild(newElement3);   
+        }
+}
+
+async function casesHogwarts(){
+    const response= await fetch(`${URL_API}/houses`);
+    const data= await response.json()
+    return data;
 }
